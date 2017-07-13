e464ce36f55bf8419e100a4af9464e3cd9721799
eriludni <erikjohanlundin@gmail.com>
2017-07-13 21:01:45 +0200

    added searchcomponentstuff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import SearchComponent from './components/SearchComponent';
+import SearchComponent from './components/SearchComponent.js';
 import './App.css';
 
 class App extends Component {
--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import Autocomplete from 'react-toolbox/lib/autocomplete';
+
+const source = {
+  'ES-es': 'Spain',
+  'TH-th': 'Thailand',
+  'EN-gb': 'England',
+  'EN-en': 'USA'
+};
 
 export default class SearchComponent extends Component {
+
+
+  handleChange (value) {
+    this.setState({});
+  };
 
   render() {
     return (
       <div className="SearchComponent">
-
+        <Autocomplete
+          direction="down"
+          selectedPosition="above"
+          label="Type an artist or film or whatevs"
+          onChange={this.handleChange}
+          source={source}/>
       </div>
     );
   }

4fb4ef0c879836456f1abbf5d0aa6c1b917b59d0
Mikael Malmqvist <mikael.malmqvist.web@gmail.com>
2017-07-13 20:33:20 +0200

    add searchcomponent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import SearchComponent from './components/SearchComponent';
 import './App.css';
 
 class App extends Component {
@@ -7,11 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get rect, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
---
+++ src/components/SearchComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class SearchComponent extends Component {
+
+  render() {
+    return (
+      <div className="SearchComponent">
+
+      </div>
+    );
+  }
+}

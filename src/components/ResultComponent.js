2c669fb60e64374bdf834c8e5154d22b96535895
eriludni <erikjohanlundin@gmail.com>
2017-07-13 22:32:57 +0200

    fixed problems with resultcomponents

---
+++ src/components/ResultComponent.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import FlatButton from 'material-ui/FlatButton';
+
+export default class ResultComponent extends Component {
+
+  render() {
+    return(
+      <div name="cardyolo">
+        <img src="../img/swh.png"/>
+      </div>
+
+
+    );
+
+  }
+}
